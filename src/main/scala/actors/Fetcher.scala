59c2fd1af6b51c022170e4a76031a88a0450fa1f
Oleksandr Korotkyi <oleksandr.korotkyi@oracle.com>
2022-05-11 23:43:10 +0300

    small refactoring

---
+++ src/main/scala/actors/Fetcher.scala
@@ -0,0 +1,8 @@
+package actors
+
+import dto.Direction
+import scala.concurrent.duration.Duration
+
+class Fetcher(direction: Direction, fetchDelay: Duration, retryDelay: Duration) {
+  
+}
--- src/main/scala/messages/Messages.scala
+++ src/main/scala/messages/Messages.scala
@@ -5,4 +5,5 @@ import models.stellartest.{Record, TransactionsQueryResult}
 sealed trait TransactionsMessage
 final case class TransactionsData(queryResult: TransactionsQueryResult) extends TransactionsMessage
 
-
+sealed trait FetchMessage
+final case class FetchTransactions(url: String)
